7c827718718fa0fd524aabaf56bab75ee63b0527
stermaneran <stermaneran@gmail.com>
2018-07-16 13:12:40 +0300

    - Added password and pastebin

--- web/public/js_codes/services/profileService.js
+++ web/public/js_codes/services/profileService.js
@@ -1,16 +1,30 @@
 BDApp.service('ProfileService', function ($http) {
 
-    this.search = function () {
-        return $http.get('/cards/get-cards')
+    this.search = function (passwd) {
+        return $http.get('/cards/get-cards?password=' + passwd)
             .then(function (data) {
                 console.log(JSON.stringify(data));
                 return data;
             })
             .catch(function () {
-                console.log("Caught error in predicting");
+                console.log("Caught error in getting cards");
+                return null;
             });
     };
-    this.search();
+    // this.search();
+
+    this.pasteBin = function() {
+
+        return $http.get('/cards/pastebin')
+            .then(function (data) {
+                console.log(JSON.stringify(data));
+                return data;
+            })
+            .catch(function () {
+                console.log("Caught error in getting cards");
+                return null;
+            });
+    }
 
 
 });
